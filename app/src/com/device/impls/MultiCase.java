cf5273004943c9d8fe10069df5edee90cf4d9fb6
sanbo.xyz <sanbo.xyz@gmail.com>
2019-08-04 21:05:11 +0800

    demo支持多进程测试

--- app/src/com/device/impls/MultiCase.java
+++ app/src/com/device/impls/MultiCase.java
@@ -24,5 +24,64 @@ public class MultiCase {
      */
     public static void runCase(Context context, int caseNum) {
         EL.i(ProcessUtils.getCurrentProcessName(context) + "--------->收到case" + caseNum);
+
+        switch (caseNum) {
+            case 1:
+                runCase1(context);
+                break;
+            case 2:
+                runCase2(context);
+                break;
+            case 3:
+                runCase3(context);
+                break;
+            case 4:
+                runCase4(context);
+                break;
+            case 5:
+                runCase5(context);
+                break;
+            case 6:
+                runCase6(context);
+                break;
+            case 7:
+                runCase7(context);
+                break;
+            case 8:
+                runCase8(context);
+                break;
+            case 9:
+                runCase9(context);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void runCase1(final Context context) {
+    }
+
+    public static void runCase2(final Context context) {
+    }
+
+    public static void runCase3(final Context context) {
+    }
+
+    public static void runCase4(final Context context) {
+    }
+
+    public static void runCase5(final Context context) {
+    }
+
+    public static void runCase6(final Context context) {
+    }
+
+    public static void runCase7(final Context context) {
+    }
+
+    public static void runCase8(final Context context) {
+    }
+
+    public static void runCase9(final Context context) {
     }
 }
--- app/src/com/device/impls/TestCasesImpl.java
+++ app/src/com/device/impls/TestCasesImpl.java
@@ -2,19 +2,14 @@ package com.device.impls;
 
 import android.content.Context;
 
-import com.analysys.track.internal.impl.AppSnapshotImpl;
-import com.analysys.track.internal.impl.oc.OCImpl;
 import com.analysys.track.internal.net.PolicyImpl;
 import com.analysys.track.internal.net.UploadImpl;
 import com.device.utils.AssetsHelper;
 import com.device.utils.EL;
-import com.device.utils.MyLooper;
 import com.device.utils.ProcessUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.List;
 
 
 /**
@@ -29,7 +24,7 @@ public class TestCasesImpl {
 
     public static void runCase(Context context, int caseNum) {
 
-        EL.i(ProcessUtils.getCurrentProcessName(context) + "--- you click  btnCase" + caseNum);
+        EL.d(ProcessUtils.getCurrentProcessName(context) + "--- you click  btnCase" + caseNum);
         switch (caseNum) {
             case 1:
                 runCase1(context);
@@ -66,83 +61,92 @@ public class TestCasesImpl {
 
     // 1. 测试发起请求，接收策略
     public static void runCase1(final Context context) {
-        MyLooper.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    PolicyImpl.getInstance(context).clearSP();
-                    UploadImpl.getInstance(context).doUploadImpl();
-
-                } catch (Throwable e) {
-                    EL.i(e);
-                }
-            }
-        });
+//        MyLooper.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    PolicyImpl.getInstance(context).clearSP();
+//                    UploadImpl.getInstance(context).doUploadImpl();
+//
+//                } catch (Throwable e) {
+//                    EL.i(e);
+//                }
+//            }
+//        });
+        MultiProcessWorker.postMultiMessages(context, 1);
     }
 
     // 2. 测试接收并处理策略
     public static void runCase2(final Context context) {
-        MyLooper.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    testSavePolicy(context);
-                } catch (Throwable e) {
-                    EL.i(e);
-                }
-            }
-        });
+//        MyLooper.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    testSavePolicy(context);
+//                } catch (Throwable e) {
+//                    EL.i(e);
+//                }
+//            }
+//        });
+        MultiProcessWorker.postMultiMessages(context, 2);
     }
 
 
     // 3. 接收新策略
     public static void runCase3(final Context context) {
-        MyLooper.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    testReceiverPolocy(context);
-                } catch (Throwable e) {
-                    EL.i(e);
-                }
-            }
-        });
+//        MyLooper.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    testReceiverPolocy(context);
+//                } catch (Throwable e) {
+//                    EL.i(e);
+//                }
+//            }
+//        });
+        MultiProcessWorker.postMultiMessages(context, 3);
     }
 
 
     // OC测试
     private static void runCase4(final Context context) {
-        MyLooper.execute(new Runnable() {
-            @Override
-            public void run() {
-                OCImpl.getInstance(context).processOC();
-            }
-        });
+//        MyLooper.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                OCImpl.getInstance(context).processOC();
+//            }
+//        });
+        MultiProcessWorker.postMultiMessages(context, 4);
 
     }
 
     //安装列表获取
     private static void runCase5(final Context context) {
-        MyLooper.execute(new Runnable() {
-            @Override
-            public void run() {
-
-                List<JSONObject> list = AppSnapshotImpl.getInstance(context).getAppDebugStatus();
-                EL.i("getAppDebugStatus:" + list);
-            }
-        });
+//        MyLooper.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                List<JSONObject> list = AppSnapshotImpl.getInstance(context).getAppDebugStatus();
+//                EL.i("getAppDebugStatus:" + list);
+//            }
+//        });
+        MultiProcessWorker.postMultiMessages(context, 5);
     }
 
     private static void runCase6(Context context) {
+        MultiProcessWorker.postMultiMessages(context, 6);
     }
 
     private static void runCase7(Context context) {
+        MultiProcessWorker.postMultiMessages(context, 7);
     }
 
     private static void runCase8(Context context) {
+        MultiProcessWorker.postMultiMessages(context, 8);
     }
 
     private static void runCase9(Context context) {
+        MultiProcessWorker.postMultiMessages(context, 9);
     }
 
     /**
