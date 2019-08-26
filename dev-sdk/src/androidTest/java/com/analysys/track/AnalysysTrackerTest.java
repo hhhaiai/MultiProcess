71b5c1d8ad324aa3f97a801f2e15fb72250be957
sanbo <sanbo.xyz@gmail.com>
2019-08-26 17:02:19 +0800

    update case

--- dev-sdk/src/androidTest/java/com/analysys/track/AnalysysTrackerTest.java
+++ dev-sdk/src/androidTest/java/com/analysys/track/AnalysysTrackerTest.java
@@ -1,16 +1,31 @@
 package com.analysys.track;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import com.analysys.track.internal.content.EGContext;
+
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class AnalysysTrackerTest {
+    Context mContext = InstrumentationRegistry.getContext();
 
     @Test
-    public void init() {
+    public void testInit() {
+        String appkey = "testappkey";
+        String channel = "custonchannel";
+        AnalysysTracker.init(mContext, appkey, channel);
+
+//        Assert.assertEquals(SystemUtils.getAppKey(mContext), appkey);
+//        Assert.assertEquals(SystemUtils.getAppChannel(mContext), channel);
     }
 
     @Test
     public void setDebugMode() {
+        AnalysysTracker.setDebugMode(true);
+        Assert.assertTrue(EGContext.FLAG_DEBUG_USER);
+        AnalysysTracker.setDebugMode(false);
+        Assert.assertFalse(EGContext.FLAG_DEBUG_USER);
     }
 }
