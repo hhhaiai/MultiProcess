6b79b740d2fd8772aa5e921e6f5add952bc3ed7c
miqt <miqingtang@163.com>
2019-11-18 16:58:57 +0800

    合并单独版本分支修改

--- dev-sdk/src/main/java/com/analysys/track/internal/content/EGContext.java
+++ dev-sdk/src/main/java/com/analysys/track/internal/content/EGContext.java
@@ -23,7 +23,6 @@ public class EGContext {
     public static String LOGTAG_INNER;
     public static boolean FLAG_DEBUG_USER = false;
 
-    public static final String KEY_ACTION_SCREEN_ON_SIZE = "SCREEN_ON_SIZE";
 
     /**
      * 广播相关
