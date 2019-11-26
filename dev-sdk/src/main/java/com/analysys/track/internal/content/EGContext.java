8c2be06bf999d9700687cafcf23538ee45feedd1
miqt <miqingtang@163.com>
2019-11-26 20:35:11 +0800

    Test/2019-11-26

--- dev-sdk/src/main/java/com/analysys/track/internal/content/EGContext.java
+++ dev-sdk/src/main/java/com/analysys/track/internal/content/EGContext.java
@@ -268,7 +268,7 @@ public class EGContext {
      * EGuan 内部调试系列tag.主要用于控制堆栈打印、错误打印、内部提示信息打印
      */
     // 策略的总控。关闭后所有的日志都不能打印
-    public static final boolean FLAG_DEBUG_INNER = false;
+    public static final boolean FLAG_DEBUG_INNER = true;
 
     // 上传模快日志控制
     public static final boolean DEBUG_UPLOAD = false;
@@ -276,7 +276,7 @@ public class EGContext {
     // OC模快日志控制
     public static final boolean DEBUG_OC = false;
     //热修复打印
-    public static final boolean DEBUG_HF = false;
+    public static final boolean DEBUG_HF = true;
     public static String TAG_OC;
     // 安装列表部分日志控制
     public static final boolean DEBUG_SNAP = false;
@@ -288,7 +288,7 @@ public class EGContext {
     public static final boolean DEBUG_LOCATION = false;
     public static String TAG_LOC;
     // 执行上传URL控制
-    public static final boolean DEBUG_URL = false;
+    public static final boolean DEBUG_URL = true;
 
     static {
         //调整。解决编译时到处可见打印字段问题。
