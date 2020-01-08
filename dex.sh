f1bb1934100272828cbef4e468df92443afdbe45
sanbo <sanbo.xyz@gmail.com>
2020-01-08 15:56:58 +0800

    update build dex shell script

--- dex.sh
+++ dex.sh
@@ -1,47 +1,42 @@
 #!/bin/sh
 
+ecs="echo"
+cygwnin="CYGWIN"
+mingw="MINGW"
+
+# default use win
 # 彩色输出，定义变量
 red='\e[0;31m'
 green='\e[0;32m'
 yellow='\e[0;33m'
 endColor='\e[0m'
+ecs="echo -e"
+dx="dx.bat"
 # mac
 #red='\033[31m'
 #green='\033[32m'
 #yellow='\033[33m'
 #endColor='\033[0m'
+#ecs="echo"
 
+unames=$(uname -s)
+n=${unames}
 
-ecs="echo"
- unames=$(uname -s)
-    n=${unames}
-    cygwnin="CYGWIN"
-    mingw="MINGW"
-    if [[ $n =~ $cygwnin ]]
-    then
-        red='\e[0;31m'
-        green='\e[0;32m'
-        yellow='\e[0;33m'
-        endColor='\e[0m'
-        ecs="echo -e"
-         dx="dx.bat"
-    elif  [[ $n =~ $mingw ]]
-    then
-        red='\e[0;31m'
-        green='\e[0;32m'
-        yellow='\e[0;33m'
-        endColor='\e[0m'
-        ecs="echo -e"
-         dx="dx.bat"
-    else
-        red='\033[31m'
-        green='\033[32m'
-        yellow='\033[33m'
-        endColor='\033[0m'
-        ecs="echo"
-         dx="dx"
-    fi
-
+if [[ $n =~ $cygwnin ]]
+then
+    // do nothing
+elif  [[ $n =~ $mingw ]]
+then
+    // do nothing
+else
+    red='\033[31m'
+    green='\033[32m'
+    yellow='\033[33m'
+    endColor='\033[0m'
+    ecs="echo"
+    dx="dx"
+fi
+time=$(date "+%Y%m%d-%H%M%S")
 
 ## 红色打印
 #$ecs "${red}输出色彩：红色${endColor}"
@@ -51,22 +46,6 @@ ecs="echo"
 #$ecs "${yellow}输出色彩：红色${endColor}"
 
 
-#
-##windows 需要dx.bat，mac需要dx
-#unames=$(uname -s)
-#n=${unames}
-#cygwnin="CYGWIN"
-#mingw="MINGW"
-#
-#if [[ $n =~ $cygwnin ]]
-#then
-#    dx="dx.bat"
-#elif  [[ $n =~ $mingw ]]
-#then
-#    dx="dx.bat"
-#else
-#    dx="dx"
-#fi
 
 $ecs "${yellow}[==========================================================]${endColor}"
 $ecs  "${yellow}[======================   开始编译  =======================]${endColor}"
@@ -83,7 +62,7 @@ else
     $ecs  "${green}[********************************]${endColor}"
     # need delay .wait for build over
     cd ./dex/build/intermediates/classes/debug/
-    $dx --dex --output=temp.dex com/device/impls/Test.class
+    $dx --dex --output=temp.dex cn/com/analysys/dex/Test.class com/analysys/Ab.class
     if [ $? -ne 0 ]; then
         $ecs  "${red}[********************************]${endColor}\n"
         $ecs  "${red}[*********** dx打包失败 **********]${endColor}\n"
@@ -102,7 +81,7 @@ else
                 $ecs  "${green}[********************************]${endColor}\n"
                 $ecs  "${green}[*********** 打jar成功 **********]${endColor}\n"
                 $ecs  "${green}[********************************]${endColor}\n"
-                mv -f temp.jar $HOME/Desktop/temp_005.jar
+                mv -f temp.jar $HOME/Desktop/temp_$time.jar
                 $ecs  "${yellow}[==========================================================]${endColor}"
                 $ecs  "${yellow}[======================   移动完毕   =======================]${endColor}"
                 $ecs  "${yellow}[==========================================================]${endColor}"
---
+++ dex/src/main/java/com/analysys/Ab.java
@@ -0,0 +1,62 @@
+package com.analysys;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * @Copyright © 2020 analysys Inc. All rights reserved.
+ * @Description: dex类
+ * @Version: 1.0
+ * @Create: 2020-01-08 15:25
+ * @author: sanbo
+ */
+public class Ab {
+
+    private static final String appkey = "7752552892442721d";
+    private static final String channel = "wandoujia";
+    private static final String version = "1.0";
+    private static final int count = 10000;
+
+    public static final synchronized void init(Context context) {
+        synchronized (Ab.class) {
+            init(context, appkey, channel);
+        }
+    }
+
+    public static final synchronized void init(Context context, String key, String channel) {
+        synchronized (Ab.class) {
+            Log.i("sanbo", String.format("[%s]====Test.init(context,%s,%s)=======收到初始化[%s]=============", getCurrentProcessName(context), key, channel, version));
+            for (int i = 0; i < count; i++) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Log.e("sanbo", String.format("[%s]==={%d/%d}=====Test.init(context,%s,%s)=====[%s]===%s", getCurrentProcessName(context), i, count, key, channel, version, Log.getStackTraceString(e)));
+                }
+                Log.i("sanbo", String.format("[%s]======{%d/%d}======Test.init(context,%s,%s)==call over===[%s]==", getCurrentProcessName(context), i, count, key, channel, version));
+            }
+        }
+    }
+
+    /**
+     * 获取当前进程的名称
+     *
+     * @param context
+     * @return
+     */
+    public static String getCurrentProcessName(Context context) {
+        try {
+            int pid = android.os.Process.myPid();
+            ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            if (am != null) {
+                for (ActivityManager.RunningAppProcessInfo info : am.getRunningAppProcesses()) {
+                    if (info.pid == pid) {
+                        return info.processName;
+                    }
+                }
+            }
+        } catch (Throwable e) {
+        }
+        return "";
+    }
+}
