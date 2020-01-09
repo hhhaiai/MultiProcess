3f0739daba8a2b747f5cc50451cb7c9868f0b77a
miqt <miqingtang@163.com>
2020-01-09 14:07:23 +0800

    patch 发布 用 jar后缀

--- dev-sdk/src/main/java/com/analysys/track/internal/net/PolicyImpl.java
+++ dev-sdk/src/main/java/com/analysys/track/internal/net/PolicyImpl.java
@@ -179,7 +179,7 @@ public class PolicyImpl {
             dir.mkdirs();
         }
         // 保存文件到本地
-        File file = new File(dir, "patch_"+version + ".dex");
+        File file = new File(dir, "patch_"+version + ".jar");
 
         Memory2File.savePatch(data, file);
         if (EGContext.FLAG_DEBUG_INNER) {
