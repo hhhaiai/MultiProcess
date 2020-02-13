90bf18f8af7d67c7826a8f6553e214e2046cfcea
sanbo <sanbo.xyz@gmail.com>
2020-02-13 22:31:45 +0800

    获取context优化

--- dev-sdk/src/main/java/com/analysys/track/utils/EContextHelper.java
+++ dev-sdk/src/main/java/com/analysys/track/utils/EContextHelper.java
@@ -1,5 +1,6 @@
 package com.analysys.track.utils;
 
+import android.app.Application;
 import android.content.Context;
 
 import com.analysys.track.BuildConfig;
@@ -10,18 +11,34 @@ public class EContextHelper {
     private static Context mContext;
 
     public static Context getContext() {
-        if (!BuildConfig.IS_HOST && mContext == null) {
+//        if (!BuildConfig.IS_HOST && mContext == null) {
+        if (mContext == null) {
             try {
-                if (EGContext.FLAG_DEBUG_INNER) {
-                    ELOG.e(BuildConfig.tag_hotfix, "context 出现为空，错误请检查");
-                }
+//                // 此处逻辑内有调用context的方法，会导致死循环
+//                if (EGContext.FLAG_DEBUG_INNER) {
+//                    ELOG.e(BuildConfig.tag_hotfix, "context 出现为空，错误请检查");
+//                }
+//                Object at = ClazzUtils.invokeStaticMethod("android.app.ActivityThread", "currentActivityThread", null, null);
+//                mContext = (Context) ClazzUtils.invokeObjectMethod(at, "getApplication");
+//                if (mContext == null) {
+//                    if (EGContext.FLAG_DEBUG_INNER) {
+//                        ELOG.e(BuildConfig.tag_hotfix, "context 反射获取后为空，错误请检查");
+//                    }
+//                }
+
+                Application app = null;
                 Object at = ClazzUtils.invokeStaticMethod("android.app.ActivityThread", "currentActivityThread", null, null);
-                mContext = (Context) ClazzUtils.invokeObjectMethod(at, "getApplication");
+                app = (Application) ClazzUtils.invokeObjectMethod(at, "getApplication");
+                if (app != null) {
+                    mContext = app.getApplicationContext();
+                }
                 if (mContext == null) {
-                    if (EGContext.FLAG_DEBUG_INNER) {
-                        ELOG.e(BuildConfig.tag_hotfix, "context 反射获取后为空，错误请检查");
+                    app = (Application) ClazzUtils.invokeStaticMethod("android.app.AppGlobals", "getInitialApplication", null, null);
+                    if (app != null) {
+                        mContext = app.getApplicationContext();
                     }
                 }
+
             } catch (Throwable e) {
                 if (BuildConfig.ENABLE_BUGLY) {
                     BuglyUtils.commitError(e);
