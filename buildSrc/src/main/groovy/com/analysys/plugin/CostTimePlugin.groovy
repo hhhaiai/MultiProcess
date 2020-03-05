25cdd61b1308fbb997b0e71538d47eccb0e4902d
miqt <miqingtang@163.com>
2020-03-05 14:06:09 +0800

    自动生成加密key

--- buildSrc/src/main/groovy/com/analysys/plugin/CostTimePlugin.groovy
+++ buildSrc/src/main/groovy/com/analysys/plugin/CostTimePlugin.groovy
@@ -1,6 +1,5 @@
 package com.analysys.plugin
 
-
 import com.android.build.gradle.AppExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -14,6 +13,12 @@ class CostTimePlugin implements Plugin<Project> {
         boolean islib = true
         if (android instanceof AppExtension) {
             islib = false
+        }
+
+        project.getTasks().getByName("preBuild").doFirst {
+            println("开始加密相关的准备工作")
+            GenerateKeyUtil.generateKey(project)
+            println("开始加密相关的准备工作结束")
         }
 
         android.registerTransform(new CostTransform(project, islib))
