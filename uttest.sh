154c36bf0aee8ad0e0887910cc967381f1e76651
miqt <miqingtang@163.com>
2020-05-25 10:00:03 +0800

    uttest 脚本 本地测试通过

--- uttest.sh
+++ uttest.sh
@@ -1,3 +1,3 @@
-gradlew.bat :dev-sdk:assembleAndroidTest
-gradlew.bat :app:assembleRelease
+./gradlew.bat :dev-sdk:assembleAndroidTest
+./gradlew.bat :app:assembleRelease
 java -jar tools/spoon/spoon.jar --test-apk ./dev-sdk/build/outputs/apk/androidTest/debug/dev-sdk-debug-androidTest.apk --apk ./app/build/outputs/apk/release/app-release.apk --output ./spoon/
