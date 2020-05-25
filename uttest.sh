ebadcfd6d3481281f8c2aa2ca42a3cdf4fbfad33
miqt <miqingtang@163.com>
2020-05-25 11:52:23 +0800

    uttest 脚本平台适配

--- uttest.sh
+++ uttest.sh
@@ -1,3 +1,30 @@
-sh gradlew :dev-sdk:assembleAndroidTest
-sh gradlew :app:assembleRelease
-sh java -jar tools/spoon/spoon.jar --test-apk ./dev-sdk/build/outputs/apk/androidTest/debug/dev-sdk-debug-androidTest.apk --apk ./app/build/outputs/apk/release/app-release.apk --output ./spoon/
+#!/usr/bin/env bash
+
+source_common() {
+    pwd=$(
+        cd $(dirname $0)
+        pwd
+    )
+    source $pwd/common.sh md
+}
+
+
+buildAndRunUT(){
+  $gw :dev-sdk:assembleAndroidTest
+  $gw :app:assembleRelease
+  java -jar tools/spoon/spoon.jar --test-apk ./dev-sdk/build/outputs/apk/androidTest/debug/dev-sdk-debug-androidTest.apk --apk ./app/build/outputs/apk/release/app-release.apk --output ./spoon/
+}
+
+mode_up()
+{
+    chmod -R 777 *
+    git config core.filemode false
+}
+
+main() {
+    mode_up
+    source_common
+    buildAndRunUT
+}
+
+main
