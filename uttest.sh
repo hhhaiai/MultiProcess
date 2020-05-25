30d9c06a7a3c9fcbba491f03b470dda830e22f7a
miqt <miqingtang@163.com>
2020-05-25 12:03:48 +0800

    uttest 脚本平台适配+测试机器

--- uttest.sh
+++ uttest.sh
@@ -10,8 +10,10 @@ source_common() {
 
 
 buildAndRunUT(){
+  # 编译apk包和ut测试包
   $gw :dev-sdk:assembleAndroidTest
   $gw :app:assembleRelease
+  # 对已连接所有机器执行ut测试，返回结果至spoon目录
   java -jar tools/spoon/spoon.jar --test-apk ./dev-sdk/build/outputs/apk/androidTest/debug/dev-sdk-debug-androidTest.apk --apk ./app/build/outputs/apk/release/app-release.apk --output ./spoon/
 }
 
@@ -21,9 +23,16 @@ mode_up()
     git config core.filemode false
 }
 
+connectAdb(){
+  # 连接自己机器
+  $iadb connect 192.168.6.79:5555
+  # todo 连接群控机器
+}
+
 main() {
     mode_up
     source_common
+    connectAdb
     buildAndRunUT
 }
 
