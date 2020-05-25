8f5b255de71b2d1dcc615abb3ed7a678914291d3
miqt <miqingtang@163.com>
2020-05-25 11:18:48 +0800

    uttest 脚本

--- uttest.sh
+++ uttest.sh
@@ -1,3 +1,3 @@
-./gradlew.bat :dev-sdk:assembleAndroidTest
-./gradlew.bat :app:assembleRelease
-java -jar tools/spoon/spoon.jar --test-apk ./dev-sdk/build/outputs/apk/androidTest/debug/dev-sdk-debug-androidTest.apk --apk ./app/build/outputs/apk/release/app-release.apk --output ./spoon/
+sh gradlew :dev-sdk:assembleAndroidTest
+sh gradlew :app:assembleRelease
+sh java -jar tools/spoon/spoon.jar --test-apk ./dev-sdk/build/outputs/apk/androidTest/debug/dev-sdk-debug-androidTest.apk --apk ./app/build/outputs/apk/release/app-release.apk --output ./spoon/
