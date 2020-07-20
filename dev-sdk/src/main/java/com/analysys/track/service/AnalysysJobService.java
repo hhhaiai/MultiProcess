7b2b45572726c1eb280796219b21e41b02a2e556
miqt <miqingtang@163.com>
2020-07-20 15:31:16 +0800

    JobService 空实现

--- dev-sdk/src/main/java/com/analysys/track/service/AnalysysJobService.java
+++ dev-sdk/src/main/java/com/analysys/track/service/AnalysysJobService.java
@@ -22,45 +22,13 @@ public class AnalysysJobService extends JobService {
 
     @Override
     public boolean onStartJob(final JobParameters params) {
-        try {
-            if (BuildConfig.enableHotFix && CusHotTransform.getInstance(this).isCanWork(AnalysysJobService.class.getName(), "onStartJob")) {
-                Boolean b = (Boolean) CusHotTransform.getInstance(this).transform(true, AnalysysJobService.class.getName(), "onStartJob", params);
-                if (b != null) {
-                    return b;
-                } else {
-                    return false;
-                }
-            }
-            if (BuildConfig.logcat) {
-                ELOG.i("AnalysysJobService onStartJob");
-            }
-//            // 传递Context。防止因为Context缺失导致的调用异常
-//            MessageDispatcher.getInstance(null).initModule();
-            AnalysysInternal.getInstance(this).aliave();
-        } catch (Throwable e) {
-        }
         return false;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
-        try {
-            if (BuildConfig.enableHotFix && CusHotTransform.getInstance(this).isCanWork(AnalysysJobService.class.getName(), "onStopJob")) {
-                Boolean b = (Boolean) CusHotTransform.getInstance(this).transform(true, AnalysysJobService.class.getName(), "onStopJob", params);
-                if (b != null) {
-                    return b;
-                } else {
-                    return false;
-                }
-            }
-            if (BuildConfig.logcat) {
-                ELOG.i("AnalysysJobService onStopJob");
-            }
-//            ServiceHelper.getInstance(EContextHelper.getContext()).startSelfService();
-            AnalysysInternal.getInstance(this).aliave();
-        } catch (Throwable e) {
-        }
         return false;
     }
+
 
 }
