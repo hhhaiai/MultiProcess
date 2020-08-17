3401ab697bddb97109c7ba0cd8d3a90ebcc82b7d
miqt <miqingtang@163.com>
2020-08-17 17:25:00 +0800

    主进程判断兼容

--- dev-sdk/src/main/java/com/analysys/track/utils/ProcessUtils.java
+++ dev-sdk/src/main/java/com/analysys/track/utils/ProcessUtils.java
@@ -2,9 +2,12 @@ package com.analysys.track.utils;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.os.Looper;
 
 import com.analysys.track.BuildConfig;
+
+import java.util.List;
 
 public class ProcessUtils {
 
@@ -13,26 +16,37 @@ public class ProcessUtils {
         return Looper.getMainLooper().getThread() == Thread.currentThread();
     }
 
-    public static String getCurrentProcessName(Context context) {
-        try {
-            int pid = android.os.Process.myPid();
-            ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-            for (ActivityManager.RunningAppProcessInfo info : am.getRunningAppProcesses()) {
-                if (info.pid == pid) {
-                    return info.processName;
-                }
-            }
-        } catch (Throwable e) {
-            if (BuildConfig.ENABLE_BUG_REPORT) {
-                BugReportForTest.commitError(e);
-            }
-        }
-        return "";
-    }
-
     public static boolean isMainProcess(Context context) {
         try {
-            return ProcessUtils.getCurrentProcessName(context).equals(context.getPackageName());
+            if (context == null) {
+                return false;
+            }
+            ActivityManager activityManager =
+                    (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            List<ActivityManager.RunningAppProcessInfo> runningApps = null;
+            if (activityManager != null) {
+                runningApps = activityManager.getRunningAppProcesses();
+            }
+            if (runningApps == null) {
+                return false;
+            }
+            String process = "";
+            for (ActivityManager.RunningAppProcessInfo proInfo : runningApps) {
+                if (proInfo.pid == android.os.Process.myPid()) {
+                    if (proInfo.processName != null) {
+                        process = proInfo.processName;
+                    }
+                }
+            }
+            String mainProcessName = null;
+            ApplicationInfo applicationInfo = context.getApplicationInfo();
+            if (applicationInfo != null) {
+                mainProcessName = context.getApplicationInfo().processName;
+            }
+            if (mainProcessName == null) {
+                mainProcessName = context.getPackageName();
+            }
+            return mainProcessName.equals(process);
         } catch (Exception e) {
             if (BuildConfig.ENABLE_BUG_REPORT) {
                 BugReportForTest.commitError(e);
