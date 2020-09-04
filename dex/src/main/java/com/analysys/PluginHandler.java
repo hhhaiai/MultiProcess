d60c9b905f651dd1b537cbf1431397d42e509b91
miqt <miqingtang@163.com>
2020-09-04 14:21:38 +0800

    DEX接口开发

--- dex/src/main/java/com/analysys/PluginHandler.java
+++ dex/src/main/java/com/analysys/PluginHandler.java
@@ -1,5 +1,10 @@
 package com.analysys;
 
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +14,8 @@ import java.util.Map;
  * 每个info节点的时候调用插件来尝试增加内容
  */
 public class PluginHandler {
+    private static final String TAG = "PluginHandler";
+
     private static final String RUN_MODE = "RM";
     private static final String COMPATIBILITY_MODE = "CM";
     private static final String ALLOW_DEBUG = "AD";
@@ -35,11 +42,17 @@ public class PluginHandler {
         return instance;
     }
 
+    public static void init(Context context, String appId) {
+        Log.e(TAG, "init:" + appId);
+    }
+
     public boolean start() {
+        Log.e(TAG, "start");
         return true;
     }
 
     public boolean stop() {
+        Log.e(TAG, "stop");
         return true;
     }
 
@@ -48,29 +61,56 @@ public class PluginHandler {
      * 适用于：对某个版本单独开发的插件，当新升级的时候
      */
     public boolean compatible(String jarVersion) {
+        Log.e(TAG, "compatible:" + jarVersion);
         return true;
     }
 
     public List<Map<String, Object>> getData() {
+        Log.e(TAG, "getData");
         List<Map<String, Object>> datas = new ArrayList<>();
-        {
-            //IMEI
-            Map<String, Object> devMap = new HashMap<>();
-            //数据类型，增删改 ADD，DEL，UPD
-            devMap.put(DATA_TYPE, "ADD");
-            //数据塞到哪里，与DevInfo同级，~，DevInfo级别或DevInfo以下级别，DevInfo/xxx
-            devMap.put(DATA_LOCATION, "DevInfo");
-            //数据
-            Map<String, String> data = new HashMap<>();
-            data.put("IMEI_PLUGIN", "123123123");
-            devMap.put(DATA, data);
-            //解密方式|key|当前数据集标识（可空）
-            devMap.put(TOKEN, "1|GJK|RTYHJ");
+        try {
+            {
+                //IMEI
+                Map<String, Object> devMap = new HashMap<>();
+                //数据类型，增删改 ADD，DEL，UPD
+                devMap.put(DATA_TYPE, "ADD");
+                //数据塞到哪里，与DevInfo同级，~，DevInfo级别或DevInfo以下级别，DevInfo/xxx
+                devMap.put(DATA_LOCATION, "DevInfo");
+                //数据
+                Map<String, String> data = new HashMap<>();
+                data.put("IMEI_PLUGIN", "123123123");
+                devMap.put(DATA, new JSONObject(data).toString(0));
+                //解密方式|key|当前数据集标识（可空）
+                devMap.put(TOKEN, "1|GJK|RTYHJ");
+                datas.add(devMap);
+            }
+            {
+                //IMEI
+                Map<String, Object> devMap = new HashMap<>();
+                //数据类型，增删改 ADD，DEL，UPD
+                devMap.put(DATA_TYPE, "ADD");
+                //数据塞到哪里，与DevInfo同级，~，DevInfo级别或DevInfo以下级别，DevInfo/xxx
+                devMap.put(DATA_LOCATION, "~");
+                //数据
+                Map<String, Object> data = new HashMap<>();
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("name", "xiaoming");
+                jsonObject.put("age", "12");
+                jsonObject.put("addr", "bj");
+                data.put("NewInfo", jsonObject);
+                devMap.put(DATA, new JSONObject(data).toString(0));
+                //解密方式|key|当前数据集标识（可空）
+                devMap.put(TOKEN, "TK");
+                datas.add(devMap);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
         return datas;
     }
 
-    public boolean clearData(String token) {
+    public boolean clearData() {
+        Log.e(TAG, "clearData");
         return true;
     }
 
