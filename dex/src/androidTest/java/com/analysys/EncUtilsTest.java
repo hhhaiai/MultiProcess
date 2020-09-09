9a4a504f588fef925b9b6ed22f8a1ca306f5e650
miqt <miqingtang@163.com>
2020-09-09 14:30:08 +0800

    调整UTcase

--- dex/src/androidTest/java/com/analysys/EncUtilsTest.java
+++ dex/src/androidTest/java/com/analysys/EncUtilsTest.java
@@ -9,9 +9,15 @@ public class EncUtilsTest {
 
     @Test
     public void enc() {
-        String[] str = new String[]{"", "hgfdhfdgh", "q", "fdskafhdsakfhsafhkdsafh", "\n"};
+        String[] str = new String[]{"", "hgfdhfdgh", "q", "fdskafhdsakfhsafhkdsafh", "\n",
+                "#$%^&*()"
+                ,"中文房" +
+                "间号的撒换个坑范德萨发货科恒股" +
+                "份公爵黑卡施工后果第三方和骄傲和","▽□◐♀×★✘▋▊卍♮▬卐‖♫《⒄" +
+                "⒂⑶⑯⑲⑤⑰⒃嬲恏氼凵冂㇏丿せつききサセココノヌチチㅙㅆㅆ㉱㉡ㄸ㉯" +
+                "㉮㉻ㄹППОСЁътббаяЯА┌╩╬╕┠╟╨╊ыэŒÀΑΒΗΤΦ "};
         for (int i = 0; i < str.length; i++) {
-            Pair pair = EncUtils.enc(str[i], 10000);
+            Pair pair = EncUtils.enc(str[i], 5);
             String data = EncUtils.dec(pair);
             Assert.assertEquals(str[i], data);
         }
