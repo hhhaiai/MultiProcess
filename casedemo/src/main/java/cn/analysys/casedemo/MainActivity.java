bc5d83f577442f023e3e0c943004083e0ab35613
sanbo <sanbo.xyz@gmail.com>
2021-03-06 10:26:47 +0800

    规整casedemo

--- casedemo/src/main/java/cn/analysys/casedemo/MainActivity.java
+++ casedemo/src/main/java/cn/analysys/casedemo/MainActivity.java
@@ -2,6 +2,7 @@ package cn.analysys.casedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnGotoTestCasePage:
+                break;
+            default:
+                break;
+        }
     }
 }
