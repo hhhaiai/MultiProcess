d5ebb766f456a2a5d102991efaaeeecddddb7821
sanbo <sanbo.xyz@gmail.com>
2021-03-19 20:06:08 +0800

    数据库语法升级

--- dev-sdk/src/main/java/com/analysys/track/db/DBType.java
+++ dev-sdk/src/main/java/com/analysys/track/db/DBType.java
@@ -8,12 +8,13 @@ package com.analysys.track.db;
  * @author: sanbo
  */
 public class DBType {
-    public static final String AUTOINCREMENT = " Integer Primary Key Autoincrement ";
+    public static final String AUTOINCREMENT = " integer Primary Key Autoincrement ";
     public static final String VARCHAR_TEN = " varchar(10) not null ";
     public static final String VARCHAR_TEN_NULL = " varchar(10) ";
     public static final String VARCHAR_TWENTY = " varchar(20)";
     public static final String VARCHAR_TWENTY_NULL = " varchar(20) ";
     public static final String VARCHAR_HUNDRED = " varchar(100) not null ";
-    public static final String INT_DEFAULT_ONE = " int default 1 ";
+    public static final String INT_DEFAULT_ONE = " integer default 1 ";
+    public static final String INT = " integer ";
     public static final String TEXT = " text ";
 }
