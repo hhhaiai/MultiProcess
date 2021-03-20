96595cb00c9dd417d6904c54df8190582dbad3fb
sanbo <sanbo.xyz@gmail.com>
2021-03-20 16:54:46 +0800

    增加部分测试case

--- dev-sdk/src/androidTest/java/com/analysys/track/internal/impl/ftime/LmFileImplTest.java
+++ dev-sdk/src/androidTest/java/com/analysys/track/internal/impl/ftime/LmFileImplTest.java
@@ -1,6 +1,7 @@
 package com.analysys.track.internal.impl.ftime;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.analysys.track.AnalsysTest;
@@ -17,13 +18,16 @@ public class LmFileImplTest extends AnalsysTest {
     Context mContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
     @Test
-    public void testTryGetFileTime() {
+    public void testRealGetFlt() {
         LmFileImpl.getInstance(mContext).realGetFlt(null);
         Map<String, Long> mem = LmFileImpl.getInstance(mContext).getMemDataForTest();
         ELOG.i(mem.toString());
         Assert.assertTrue(mem.size() > 0);
     }
 
+    @Test
     public void testGetInstance() {
+        Assert.assertEquals(LmFileImpl.getInstance(mContext), LmFileImpl.getInstance(mContext));
     }
+
 }
---
+++ dev-sdk/src/androidTest/java/com/analysys/track/internal/impl/ftime/LmFileUitlsTest.java
@@ -0,0 +1,35 @@
+package com.analysys.track.internal.impl.ftime;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.analysys.track.AnalsysTest;
+import com.analysys.track.utils.ELOG;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+
+public class LmFileUitlsTest extends AnalsysTest {
+    Context mContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+
+    @Test
+    public void testGetLastAliveTimeInBaseDir() {
+        List<LmFileUitls.AppTime> list = LmFileUitls.getLastAliveTimeInBaseDir(mContext);
+        ELOG.i("==========testGetLastAliveTimeInBaseDir: %d================", list.size());
+        ELOG.i(list.toString());
+        Assert.assertTrue(list.size() > 0);
+    }
+
+    @Test
+    public void testGetLastAliveTimeInSD() {
+        List<LmFileUitls.AppTime> list = LmFileUitls.getLastAliveTimeInSD(mContext);
+        ELOG.i("=========testGetLastAliveTimeInSD: %d================", list.size());
+        ELOG.i(list.toString());
+        Assert.assertTrue(list.size() > 0);
+    }
+}
