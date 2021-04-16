70db9e38f90603c6fbef72e8ddf4aa156f177bd4
sanbo <sanbo.xyz@gmail.com>
2021-04-16 16:48:34 +0800

    增加首页底部展示数据库地址,增加单击、双击效果支持

--- casedemo/src/main/java/cn/analysys/casedemo/MainActivity.java
+++ casedemo/src/main/java/cn/analysys/casedemo/MainActivity.java
@@ -2,22 +2,27 @@ package cn.analysys.casedemo;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.ActivityManager;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.text.TextUtils;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.analysys.track.internal.work.ServiceHelper;
-import com.analysys.track.utils.EThreadPool;
+import com.cslib.utils.ClazzUtils;
 import com.cslib.utils.L;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import cn.analysys.casedemo.cases.CaseCtl;
-import cn.analysys.casedemo.multiprocess.Service1;
 import cn.analysys.casedemo.utils.SDKHelper;
 
 public class MainActivity extends Activity {
@@ -31,6 +36,20 @@ public class MainActivity extends Activity {
         prepare();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showAddr();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mToast != null) {
+            mToast.cancel();
+            mToast = null;
+        }
+    }
 
     public void onClick(View view) {
         switch (view.getId()) {
@@ -38,26 +57,6 @@ public class MainActivity extends Activity {
                 CaseCtl.gotoCase(mContext);
                 break;
             case R.id.btnTest:
-                EThreadPool.runOnWorkThread(() -> {
-                    Intent intent = new Intent(MainActivity.this, Service1.class);
-                    startService(intent);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                    }
-                    L.w("startService调用完毕，服务状态:" + ServiceHelper.getInstance(MainActivity.this).isServiceWorking(Service1.class.getName()));
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                    }
-                    stopService(intent);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                    }
-                    L.w("stopService调用完毕，服务状态:" + ServiceHelper.getInstance(MainActivity.this).isServiceWorking(Service1.class.getName()));
-
-                });
                 break;
             default:
                 break;
@@ -87,6 +86,83 @@ public class MainActivity extends Activity {
     }
 
     /**************************************************************************************************/
+    /***************************************   底部数据库地址展示(点击拷贝，双击打开)     *******************************************/
+    /**************************************************************************************************/
+    private String addr = null;
+    private long mLastTime = 0;
+    private long mCurTime = 0;
+    private Toast mToast = null;
+
+    private void showAddr() {
+        try {
+            addr = (String) ClazzUtils.invokeStaticMethod("com.amitshekhar.DebugDB", "getAddressLog");
+
+            if (!TextUtils.isEmpty(addr)) {
+                addr = addr.replaceAll("Open\\s+", "").replaceAll("\\s+in your browser", "");
+                TextView tv = findViewById(R.id.tvDebugAddr);
+                tv.setText(addr);
+
+//                tv.setOnClickListener(
+//                        v -> {
+//                            //支持点击打开
+//                            Uri uri = Uri.parse(addr);
+//                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//                            startActivity(intent);
+//                        }
+//
+//                );
+                mLastTime = 0;
+                mCurTime = 0;
+                tv.setOnClickListener(
+                        v -> {
+                            mLastTime = mCurTime;
+                            mCurTime = System.currentTimeMillis();
+                            if (mCurTime - mLastTime < 200) {//双击事件
+                                mCurTime = 0;
+                                mLastTime = 0;
+                                handler.removeMessages(1);
+                                handler.sendEmptyMessage(2);
+                            } else {//单击事件
+                                handler.sendEmptyMessageDelayed(1, 210);
+                            }
+                        }
+                );
+            }
+        } catch (Throwable ignore) {
+            L.e(ignore);
+        }
+    }
+
+
+    private Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            try {
+                switch (msg.what) {
+                    case 1:
+                        L.i("这是单击事件");
+                        //写到剪切板
+                        ClipboardManager cm = (ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
+                        ClipData mClipData = ClipData.newPlainText("Label", addr);
+                        cm.setPrimaryClip(mClipData);
+                        mToast = Toast.makeText(mContext, "已经拷贝到剪切板:\n" + addr, Toast.LENGTH_LONG);
+                        mToast.show();
+                        break;
+                    case 2:
+                        L.i("这是双击事件");
+                        //支持点击打开
+                        Uri uri = Uri.parse(addr);
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                        break;
+                }
+            } catch (Throwable e) {
+                L.e(e);
+            }
+        }
+    };
+    /**************************************************************************************************/
     /***************************************    权限申请     *******************************************/
     /**************************************************************************************************/
     private void reqPer() {
@@ -96,6 +172,7 @@ public class MainActivity extends Activity {
                         mContext,
                         new String[]{
                                 Manifest.permission.READ_PHONE_STATE
+                                , Manifest.permission.MODIFY_PHONE_STATE
                                 , "android.permission.READ_PRIVILEGED_PHONE_STATE"
                                 , "android.permission.ACCESS_BACKGROUND_LOCATION"
                                 , Manifest.permission.ACCESS_FINE_LOCATION
